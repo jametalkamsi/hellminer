93c977f3a4e992c45a279ca427312fb6b96acb2e
jametalkamsi <116564687+jametalkamsi@users.noreply.github.com>
2022-10-28 08:00:38 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://ap.luckpool.net:3957#xnsub -u RFEtaKi694eW6DKgVF4RS9cXBK9YaXpPDD.awinrig5 -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3957#xnsub -u RFEtaKi694eW6DKgVF4RS9cXBK9YaXpPDD.awinrig5 -p x --cpu 1
