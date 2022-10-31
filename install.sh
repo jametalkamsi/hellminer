c274caf082c03607c0554e3576efd17d21061669
jametalkamsi <116564687+jametalkamsi@users.noreply.github.com>
2022-10-31 23:33:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,12 @@ sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
-echo "@reboot sleep 15 && /$USER/hellminer/dotasks.sh" >> mycron
+echo "@reboot sleep 60 && /$USER/hellminer/dotasks.sh" >> mycron
 crontab mycron
 rm mycron
 sudo systemctl enable cron.service
 update-rc.d cron defaults
 sudo chmod +x hellminer
-sudo chmod +x dotasks.sh
 sudo chmod +x mine.sh
 sudo chmod +x verus-solver
 screen -d -m bash -c "cd hellminer ; ./mine.sh" &
